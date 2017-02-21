785071878bc6d61423dd5c7453de05c4f40d8af3
Mash <thebee57@gmail.com>
2017-02-21 22:01:19 +0000

    Added asteroid with collision

---
+++ Assets/Scripts/AsteroidMotion.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class AsteroidMotion : MonoBehaviour {
+
+	public float minSpeed = 1;
+	public float maxSpeed = 1;
+	private Rigidbody2D rb2d;
+	void Awake() {
+		rb2d = GetComponent<Rigidbody2D>();
+	}
+	void Start () {
+		float randomSpeed = Random.Range(minSpeed, maxSpeed);
+		float randomDirection = Random.Range(0, 2*Mathf.PI);
+		
+		var velocity = new Vector2(Mathf.Sin(randomDirection), Mathf.Cos(randomDirection));
+		velocity *= randomSpeed;
+
+		rb2d.velocity = velocity;
+	}
+}
