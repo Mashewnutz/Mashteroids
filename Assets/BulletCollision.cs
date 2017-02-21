94911797d332d098e60ada815abb4e3e0fcc45f6
Mash <thebee57@gmail.com>
2017-02-21 22:39:30 +0000

    Bullets destroy asteroids

---
+++ Assets/BulletCollision.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class BulletCollision : MonoBehaviour {
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if(other.gameObject.tag == "Bullet"){
+			Destroy(gameObject);
+		}		
+	}
+}
