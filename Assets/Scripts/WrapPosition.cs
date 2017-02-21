77ad102cc2670115cb48ab98d7c974e9fcd7d9b2
Mash <thebee57@gmail.com>
2017-02-21 22:16:23 +0000

    Wrap screen position

---
+++ Assets/Scripts/WrapPosition.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class WrapPosition : MonoBehaviour {
+				
+	void LateUpdate () {
+		var screenPosition = Camera.main.WorldToScreenPoint(transform.position);
+		if(screenPosition.x < 0){
+			screenPosition.x += Screen.width;
+			SetScreenPosition(screenPosition);			
+		} else if(screenPosition.x > Screen.width){
+			screenPosition.x -= Screen.width;
+			SetScreenPosition(screenPosition);
+		} 
+		
+		if(screenPosition.y < 0){			
+			screenPosition.y += Screen.height;
+			SetScreenPosition(screenPosition);
+		} else if(screenPosition.y > Screen.height){
+			screenPosition.y -= Screen.height;
+			SetScreenPosition(screenPosition);
+		}
+	}
+
+	void SetScreenPosition(Vector3 screenPosition){
+		var worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
+		transform.position = worldPosition;
+	}
+}
