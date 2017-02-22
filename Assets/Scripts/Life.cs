a50a05463f1ebd1e06d301fa76381fa7eaea6bc7
Mash <thebee57@gmail.com>
2017-02-22 09:06:17 +0000

    Added lives ui

---
+++ Assets/Scripts/Life.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Life : MonoBehaviour {
+
+	private GameObject lifeFilled;
+	
+	// Use this for initialization
+	void Awake () {
+		lifeFilled = transform.GetChild(0).gameObject;
+	}
+	
+	// Update is called once per frame
+	public void SetFilled (bool filled) {
+		lifeFilled.SetActive(filled);
+	}
+}
---
+++ Assets/Scripts/Lives.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lives : MonoBehaviour {
+
+	private Life[] lifes;
+	private int lives = 3;
+	void Awake() {
+		lifes = transform.GetComponentsInChildren<Life>();
+	}
+
+	public void OnPlayerDeath() {
+		lives--;	
+		for(int i = 0; i < lifes.Length; ++i){			
+			lifes[i].SetFilled(i < lives);
+		}		
+	}
+}
