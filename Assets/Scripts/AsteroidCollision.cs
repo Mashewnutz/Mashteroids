251530147947e3bd0501bbe1cc1f926b14b0ecb2
Mash <thebee57@gmail.com>
2017-02-22 21:01:30 +0000

    Splitting asteroids

--- Assets/Scripts/AsteroidCollision.cs
+++ Assets/Scripts/AsteroidCollision.cs
@@ -1,19 +1,38 @@
 ﻿using UnityEngine;
 
 public class AsteroidCollision : MonoBehaviour {
+	public GameObject prefabToSpawn;
 	private GameEvents gameEvents;
+	public int explosionForce = 5;
 	void Awake() {
-		gameEvents = FindObjectOfType<GameEvents>();
+		gameEvents = FindObjectOfType<GameEvents>();		
 	}
-	void OnCollisionEnter2D(Collision2D other)
+	void OnCollisionEnter2D(Collision2D collision)
 	{
-		if(other.gameObject.tag == "Bullet"){
+		if(collision.gameObject.tag == "Bullet"){
+			Explode(collision);
 			Destroy(gameObject);
-			Destroy(other.gameObject);
+			Destroy(collision.gameObject);
 			gameEvents.AsteroidDestroyed.Invoke();
-		} else if(other.gameObject.tag == "Player"){
-			Destroy(other.gameObject);
+		} else if(collision.gameObject.tag == "Player"){
+			Destroy(collision.gameObject);
 			gameEvents.PlayerDeath.Invoke();
 		}
+	}
+
+	void Explode(Collision2D collision){	
+		if(prefabToSpawn == null)	
+			return;
+
+		var asteroid1 = Instantiate(prefabToSpawn, transform.position, Quaternion.identity);
+		var asteroid2 = Instantiate(prefabToSpawn, transform.position, Quaternion.identity);
+
+		float xVel = -collision.contacts[0].normal.y;
+		float yVel = collision.contacts[0].normal.x;
+		Vector2 velocity = new Vector2(xVel, yVel);
+		velocity.Normalize();
+		velocity *= explosionForce;
+		asteroid1.GetComponent<Rigidbody2D>().velocity = velocity;
+		asteroid2.GetComponent<Rigidbody2D>().velocity = -velocity;
 	}
 }
