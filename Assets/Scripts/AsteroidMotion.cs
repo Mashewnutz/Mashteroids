3aa54962684ea274c6b6182a9e20345023c09f2a
Mash <thebee57@gmail.com>
2017-02-22 17:51:31 +0000

    Use fixed spawn positions for the asteroids. And set their velocities

--- Assets/Scripts/AsteroidMotion.cs
+++ Assets/Scripts/AsteroidMotion.cs
@@ -14,10 +14,9 @@ public class AsteroidMotion : MonoBehaviour {
 	}
 	void Start () {
 		float randomSpeed = Random.Range(minSpeed, maxSpeed);
-		float randomDirection = Random.Range(0, 2*Mathf.PI);
-		
-		var velocity = new Vector2(Mathf.Sin(randomDirection), Mathf.Cos(randomDirection));
-		velocity *= randomSpeed;
+		var velocity = Vector3.zero-transform.position;
+		velocity.Normalize();
+		velocity *= randomSpeed;			
 
 		rb2d.velocity = velocity;
 		rb2d.angularVelocity = Random.Range(minRotationSpeed, maxRotationSpeed);
---
+++ Assets/Scripts/AsteroidSpawner.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class AsteroidSpawner : MonoBehaviour {
+
+	public GameObject largeAsteroidPrefab;
+	public GameObject spawnPositions;
+	private int level = 0;
+
+	void Update() {
+		int asteroidCount = GameObject.FindGameObjectsWithTag("Asteroid").Length;
+		if(asteroidCount == 0){
+			level++;
+			int newAsteroidCount = GetNumberOfAsteroids(level);
+			SpawnAsteroidsAtRandomPositions(newAsteroidCount);
+		}		
+	}
+
+	void SpawnAsteroidsAtRandomPositions(int count) {
+		for(int i = 0; i < count; ++i){
+			var worldPos = GetRandomAsteroidPosition();
+			Instantiate(largeAsteroidPrefab, worldPos, Quaternion.identity);
+		}			
+	}
+
+	int GetNumberOfAsteroids(int level){
+		return (int)(Mathf.Log (level, 2) + 1);
+	}
+
+	Vector3 GetRandomAsteroidPosition(){
+		int childCount = spawnPositions.transform.childCount;
+		int randomIndex = Random.Range(0, childCount);
+		return spawnPositions.transform.GetChild(randomIndex).position;
+	}
+}
