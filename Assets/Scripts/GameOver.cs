ec6652e292a84ab27697b5e196ce3321ce9c45e3
Mash <thebee57@gmail.com>
2017-02-22 09:23:35 +0000

    Added game over text

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class GameOver : MonoBehaviour {
+	
+	void Start() {		
+		gameObject.SetActive(false);
+	}
+
+	public void OnGameOver() {		
+		gameObject.SetActive(true);		
+	}
+}
