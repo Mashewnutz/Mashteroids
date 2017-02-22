8f9484fcb7eea58d6789b012a049eebc819481a0
Mash <thebee57@gmail.com>
2017-02-22 20:17:47 +0000

    Added score

---
+++ Assets/Score.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+	public int largeAsteroid = 20;
+	public int mediumAsteroid = 50;
+	public int smallAsteroid = 100;	
+	private Text text;
+	private int score = 0;
+	void Awake() {
+		text = GetComponent<Text>();
+		UpdateText();
+	}
+	public void OnAsteroidDestroyed(){
+		score += largeAsteroid;
+		UpdateText();
+	}
+
+	void UpdateText(){
+		text.text = score.ToString();
+	}
+}
